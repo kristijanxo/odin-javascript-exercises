989a9d47d9179c48342d85b95222eecdf42ae57f
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-10-15 18:22:38 +0200

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  if (Number.isInteger(a) === false || Number.isInteger(b) === false) {
+    return "ERROR";
+  } else if (a < 0 || b < 0) {
+    return "ERROR";
+  } else {
+    const sortedArgs = [a, b].sort((a, b) => a - b);
+    let range = [];
+    for (let i = sortedArgs[0]; i <= sortedArgs[1]; i++) {
+      range.push(i);
+    }
+    return range.reduce((sum, currentValue) => sum + currentValue, 0);
+  }
 };
 
 // Do not edit below this line
