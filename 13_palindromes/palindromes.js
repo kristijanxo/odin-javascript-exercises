8e8c5031e5761df78c31142cda8be3bdfda22861
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-11-09 15:46:00 +0100

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const punctiuations = [" ", ".", ",", ":", ";", "!", "?"];
 
+  const lettersOnly = string
+    .split("")
+    .filter((char) => !punctiuations.includes(char))
+    .join("")
+    .toLowerCase();
+
+  const lettersOnlyReversed = lettersOnly
+    .slice()
+    .split("")
+    .reverse()
+    .join("")
+    .toLowerCase();
+
+  return lettersOnly === lettersOnlyReversed;
 };
+
+console.log(palindromes("dinmueter, die fetti, isch so fett!!!!"));
 
 // Do not edit below this line
 module.exports = palindromes;
