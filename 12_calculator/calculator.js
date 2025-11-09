e7a184357a27badc20b5d9c1f40cdce6a693b687
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-11-09 14:38:35 +0100

    Solve 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,14 +1,32 @@
-const add = function () {};
+const add = function (a, b) {
+  return a + b;
+};
 
-const subtract = function () {};
+const subtract = function (a, b) {
+  return a - b;
+};
 
-const sum = function () {};
+const sum = function (arr) {
+  return arr.reduce((sum, currentValue) => sum + currentValue, 0);
+};
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.reduce((acc, currentValue) => acc * currentValue, 1);
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b;
+};
 
-const factorial = function () {};
+const factorial = function (n) {
+  if (n <= 1) {
+    return 1;
+  } else if (n === 2) {
+    return 2;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
 
 // Do not edit below this line
 module.exports = {
